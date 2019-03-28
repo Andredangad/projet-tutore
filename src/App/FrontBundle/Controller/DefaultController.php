bafdfa037ef9a93da5ee0ec7999e6151fca0119a
Andredangad <andre.dang@hotmail.fr>
2019-03-28 17:18:13 +0100

    Routes

--- src/App/FrontBundle/Controller/DefaultController.php
+++ src/App/FrontBundle/Controller/DefaultController.php
@@ -6,9 +6,20 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
+<<<<<<< HEAD
 
       return $this->render('fr/accueil/index.html.twig');
 
+=======
+  		$foo = $request->get('langue');
+		if ($foo == "login"){
+        return $this->render('fr/accueil/index.html.twig');
+			
+		}
+		else
+      return $this->render($foo.'/accueil/index.html.twig');
+    }
+>>>>>>> parent of a7775bf... Changement routes
 }
 
     public function langueAction(Request $request)
